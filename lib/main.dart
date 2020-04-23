ea229bd7c3d00931b99809de1662125bd6eddb83
Stephen <sa@sc.rr.com>
2020-04-23 18:52:56 -0400

    Initial Commit Flutter App to roll dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.green.shade800,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.green.shade900,
         ),
         body: DicePage(),
       ),
@@ -15,9 +16,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDieNum = 6;
+  int rightDieNum = 6;
+  void rollDice() {
+    setState(() {
+      leftDieNum = Random().nextInt(6) + 1;
+      rightDieNum = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDieNum.png'),
+              onPressed: () {
+                rollDice();
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDieNum.png'),
+              onPressed: () {
+                rollDice();
+                print('Right button was pressed');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
